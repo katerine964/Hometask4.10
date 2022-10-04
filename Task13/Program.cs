3e47ac3d35d5a342c31c6bd0b3a1c6b569bbaaee
katerine964 <54154276+katerine964@users.noreply.github.com>
2022-10-04 12:26:24 +0300

    Add files via upload

---
+++ Task13/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+Console.WriteLine("введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n>99)
+    Console.WriteLine("Третья цифра: "+n.ToString()[2]);
+else
+{
+    Console.WriteLine("третьей цифры нет");
+}
