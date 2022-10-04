14acf9b7c11f88745390e29d9c06f35536bab04d
katerine964 <54154276+katerine964@users.noreply.github.com>
2022-10-04 12:24:08 +0300

    Add files via upload

---
+++ Task10/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите трехзначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Вторая цифра числа: "+(n/10)%10);
