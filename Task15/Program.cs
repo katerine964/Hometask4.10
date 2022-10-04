f799c108b4b1a5cedb5c6d30cbf54c07bf2b0746
katerine964 <54154276+katerine964@users.noreply.github.com>
2022-10-04 12:36:16 +0300

    Add files via upload

---
+++ Task15/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите цифру");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n>=1 && n<=7)
+{
+    if(n<6)
+    {
+       Console.WriteLine("нет"); 
+    }
+    else
+    {
+        Console.WriteLine("да");
+    }
+}
+else
+{
+    Console.WriteLine("Такого дня недели нет");
+}
